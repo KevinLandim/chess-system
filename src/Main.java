6a8dfebdbddc59fb52f840810059455160f299c2
KevinLandim <kevinb.landim@gmail.com>
2023-12-30 08:50:03 -0300

    start to implement board and piece

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
+import boardgame.Board;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello world!");
-        System.out.println("");
+        Board board = new Board(8,8);
     }
 }
