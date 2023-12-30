1bfefab0b56979abd494ae20bb56dcd6ae562632
KevinLandim <kevinb.landim@gmail.com>
2023-12-30 08:16:56 -0300

    class position created

---
+++ src/boardgame/Position.java
@@ -0,0 +1,29 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row,int column){
+        this.row=row;
+        this.column=row;
+    }
+
+    public int getRow(){
+        return this.row;
+    }
+    public int getColumn(){
+        return this.column;
+    }
+    public void setRow(int row){
+        this.row=row;
+    }
+    public void setColumn(int column){
+        this.column=column;
+    }
+    @Override
+    public String toString(){
+        return row +", "+ column;
+    }
+
+}
